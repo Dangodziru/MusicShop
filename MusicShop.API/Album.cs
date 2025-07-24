c57c036dafc8910e0cd643e54e3b091ae92b4f04
DESKTOP-CK2CUKH\Goida <beregovoyolegber@yndex.ru>
2025-07-24 11:50:14 +0300

    Добавлен сам проект, добавлены методы для работы с альбомами.

---
+++ MusicShop.API/Album.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicShop.API
+{
+    public class Album
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ MusicShop.API/Controllers/AlbumController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SQLite;
+
+namespace MusicShop.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlbumController(ILogger<WeatherForecastController> logger) : ControllerBase
+    {
+        const string dbPath = "C:\\Users\\Goida\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db";
+        const string connectionString = $"Data Source={dbPath};Version=3;";
+
+        [HttpGet("All")]
+        public List<Album> GetAll()
+        {
+
+            var list = new List<Album>();
+
+            // Создание подключения
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Album";
+
+                using (var cmd = new SQLiteCommand(sql, connection))
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+
+                        list.Add(new Album { Id = (long)reader["AlbumId"], Title = (string)reader["Title"] });
+
+                    }
+                }
+
+            }
+
+            return list;
+
+        }
+
+        [HttpGet]
+        public Album? Get(long albumId)
+        {
+
+            Album? album = null;
+
+
+            // Создание подключения
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Album WHERE AlbumId =" + albumId;
+
+                using (var cmd = new SQLiteCommand(sql, connection))
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+
+                        album = new Album { Id = (long)reader["AlbumId"], Title = (string)reader["Title"] };
+
+                    }
+                }
+
+            }
+
+            return album;
+
+        }
+
+        [HttpGet("Search")]
+        public List<Album> Search(string titleSearch)
+        {
+
+            var list = new List<Album>();
+
+            // Создание подключения
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Album WHERE Title LIKE '%" + titleSearch + "%'";
+
+                using (var cmd = new SQLiteCommand(sql, connection))
+                using (SQLiteDataReader reader = cmd.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+
+                        list.Add(new Album { Id = (long)reader["AlbumId"], Title = (string)reader["Title"] });
+
+                    }
+                }
+
+            }
+
+            return list;
+        }
+    }
+}
